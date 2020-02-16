36e2367f3e09b59cd58015845616d99146d4b346
Emmanuel Gomes <aeag@outlook.com>
2020-02-16 19:39:27 -0300

    InstallerHelper

--- TestNinja.UnitTest/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTest/Mocking/InstallerHelperTests.cs
@@ -1,14 +1,56 @@
 ﻿using NUnit.Framework;
+using Moq;
+using TestNinja.Mocking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace TestNinja.UnitTest.Mocking
 {
     [TestFixture]
     public class InstallerHelperTests
     {
+        private Mock<IDowloader> _mockDownloader;
+        private InstallerHelper _installer;
+
+        [SetUp]
+        public void Init()
+        {
+            _mockDownloader = new Mock<IDowloader>();
+            _installer = new InstallerHelper(_mockDownloader.Object);
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            _mockDownloader = null;
+            _installer = null;
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            var result = _installer.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            /* _mockDownloader.Setup(d => d.Download("","")).Throws<WebException>(); */ // Test case tests only with these values "".
+            /* Using Moq Framework */
+            _mockDownloader.Setup(d => 
+                d.Download(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installer.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
     }
 }
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -5,17 +5,45 @@ namespace TestNinja.Mocking
     public class InstallerHelper
     {
         private string _setupDestinationFile;
+        private readonly IDowloader _dowloader;
+
+        public InstallerHelper(IDowloader dowloader)
+        {
+            _dowloader = dowloader;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
+        {
+            try
+            {
+                _dowloader.Download(
+                    string.Format("http://example.com/{0}/{1}",
+                    customerName,
+                    installerName), 
+                    _setupDestinationFile);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+    }
+
+    public interface IDowloader
+    {
+        bool Download(string url, string path);
+    }
+
+    public class Downloader : IDowloader
+    {
+        public bool Download(string url, string path)
         {
             var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
+                client.DownloadFile(url, path);
 
                 return true;
             }
